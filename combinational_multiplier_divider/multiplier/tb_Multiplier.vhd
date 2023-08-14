93b6007081b90ea6c9bba1bbb5570e0d930a3f52
Omnia Mohamed <110364388+OmniaMohamed12@users.noreply.github.com>
2023-08-14 15:39:02 +0300

    Update tb_Multiplier.vhd

--- combinational_multiplier_divider/multiplier/tb_Multiplier.vhd
+++ combinational_multiplier_divider/multiplier/tb_Multiplier.vhd
@@ -14,30 +14,34 @@ architecture test of tb_Multiplier_Divider is
 component multiplier_divider_comb is 
 generic(N   : integer:=4);
     port(a, b: in signed(N-1 downto 0);
-         control: in bit;
+         control: in std_logic;
          m, r: out signed(N-1 downto 0);
-         error, busy, valid: out bit);
+          busy, valid,error: out std_logic);
 end component multiplier_divider_comb;
 signal N : integer := 4;
 for dut: multiplier_divider_comb  USE ENTITY work.multiplier_divider_comb (comb_multiplier);
 signal a_tb,b_tb:signed(N-1 downto 0);
-signal control_tb:bit;
+signal control_tb:std_logic;
 signal m_tb,r_tb:signed(N-1 downto 0);
-signal error_tb,valid_tb,busy_tb:bit;
+signal error_tb,valid_tb,busy_tb:std_logic;
+
 begin
   
-  DUT:multiplier_divider_comb generic map ( N=>4) port map(a_tb,b_tb,control_tb,m_tb,r_tb,error_tb,busy_tb,valid_tb);
+  DUT:multiplier_divider_comb generic map ( N=>4) port map(a_tb,b_tb,control_tb,m_tb,r_tb,busy_tb,valid_tb,error_tb);
   p1:process 
     FILE test_file: text OPEN READ_MODE IS "multiplier_comb_test_input.txt";
 	FILE results_file: text OPEN WRITE_MODE IS "multiplier_comb_test_results.txt";
     VARIABLE input_line: line; 
     VARIABLE results_line: line;	
     VARIABLE a_var,b_var: signed(N-1 downto 0);
-    VARIABLE control_var,error_var,valid_var,busy_var:bit;
+    VARIABLE control_var,error_var,valid_var,busy_var:std_logic;
 	VARIABLE r_var,m_var: signed(N-1 downto 0);
     VARIABLE c: character;
     VARIABLE pause: time;  
-	VARIABLE message: string (1 TO 24);
+	VARIABLE message: string (1 TO 30);
+    variable total_testcases:integer:=0;
+    variable passed_testcases:integer:=0;
+
  begin
     WHILE NOT endfile(test_file) LOOP
     READLINE (test_file, input_line);
@@ -85,11 +89,13 @@ begin
           WRITE (results_line, message);
 	  ELSE
           WRITE (results_line, string'(". Test passed."));
+		  passed_testcases:=passed_testcases+1;
 	  END IF;
-	
+	   total_testcases:=total_testcases+1;
 	  WRITELINE (results_file, results_line); 
 	
   end loop;
+report "Combinational Multiplier :Total passed testcases:   " & integer'image(passed_testcases)&'/'&integer'image(total_testcases);
   wait;
 
   
